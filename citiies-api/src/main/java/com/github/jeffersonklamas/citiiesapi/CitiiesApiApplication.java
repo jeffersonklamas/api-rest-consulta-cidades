712233c786766c24350dc6d666f3db38ff978d4a
jeffersonklamas <jeffersonklamas@gmail.com>
2021-06-03 11:43:03 -0300

    firts commit

---
+++ citiies-api/src/main/java/com/github/jeffersonklamas/citiiesapi/CitiiesApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.jeffersonklamas.citiiesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiiesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiiesApiApplication.class, args);
+	}
+
+}
---
+++ citiies-api/src/test/java/com/github/jeffersonklamas/citiiesapi/CitiiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jeffersonklamas.citiiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
